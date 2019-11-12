da7e35fc49e18656efa6c2be71f696b56cf29b78
damidrevid <oludre@gmail.com>
2019-11-12 11:31:10 +0100

    Blog 'click to read more' link

--- damiBlog/classBlog/urls.py
+++ damiBlog/classBlog/urls.py
@@ -3,4 +3,6 @@ from . import views # The dot (.) indicate the current directory.
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'), #Django automatically assign a primary key PK for every post even though we didn't indicate this while creating our model.
+                                                                   #Now we go to Views to create post.detail
 ]
--- damiBlog/classBlog/views.py
+++ damiBlog/classBlog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404 #We imported 'get_object_or_404' to control what's displayed when onbect is not available
 from django.utils import timezone
 from .models import Post
 
@@ -7,3 +7,7 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request,'classBlog/post_list.html', {'posts':posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'classBlog/post_detail.html', {'reveal':post})
